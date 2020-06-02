85377852a6eb4c12eeb25e5c0fbd1ecd148831d1
Lkhagva-erdene <anidas.com@gmail.com>
2020-06-02 21:54:04 +0300

    Add files via upload

--- JavaCollections6.java
+++ JavaCollections6.java
@@ -1,15 +1,38 @@
 import java.util.*;
 public class JavaCollections6 {
     public static void main(String[] args){
-        Set<Integer> s = new HashSet<>();
-        s.add(1);
-        System.out.println(hasOdd(s));
+        List<String> s = new ArrayList<>();
+        s.add("foo");
+        s.add("buzz");
+        s.add("bar");
+        s.add("bar");
+        s.add("bar");
+        s.add("spoon");
+        s.add("!");
+        s.add("dude");
+        System.out.println(contains3(s));
+        //removeEvenLength(s);
+
+
+       // System.out.println(hasOdd(s));
     }
-    public static boolean hasOdd(Set<Integer> set) {
-        for(Integer i: set){
-            if(i%2==1) return true;
+    public static boolean contains3(List<String> list) {
+        Map<String, Integer> MapS = new HashMap<>();
+        for(String s: list){
+            if(MapS.containsKey(s)){
+                int count = MapS.get(s);
+                MapS.put(s,count+1);
+            }else{
+                MapS.put(s,1);
+            }
         }
-        return false;
+        for(String s: MapS.keySet()){
+            int count = MapS.get(s);
+            if(count>2){
+                return true;
+            }
+        }
+        return  false;
     }
 
 }
